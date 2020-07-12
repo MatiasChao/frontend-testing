750187f27a6959065829323bf651fc2628585e2d
MatiasChao <matias.chao@despegar.com>
2020-07-12 20:15:26 -0300

    add google maps

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,9 +1,16 @@
 import React, { useEffect, useState } from 'react'
 import axios from 'axios'
+import credentials from '../credentials'
+import GoogleMapReact from 'google-map-react'
+import Marker from './maps/Marker'
 
 const Home = () => { 
-
     const [user, setUser] = useState(null)
+    const [center, setCenter] = useState({lat: -34.9032800, lng: -56.1881600 });
+    const [zoom, setZoom] = useState(12)
+    const [lat, setLat] = useState(null)
+    const [lng, setLng] = useState(null)
+
 
     useEffect(() => {
         //getUserInfo()
@@ -27,12 +34,22 @@ const Home = () => {
             console.log('ERROR')
         })
     }
+        
+   const _onClick = ({lat, lng, event}) => {
+       console.log(lat, lng, event) 
+       setLat(lat)
+       setLng(lng)
+
+       searchRestaurantsByCoordinates(lat, lng)
+    }  
+
+    // PEGARLE AL BACK ASI ME TRAER LOS RESTAURANTES...
+    const searchRestaurantsByCoordinates = ({lat, lng}) => {
+
+    }
 
     return (
         <div>
-            {
-                localStorage.getItem('user-token')
-            }
             {
                 user &&
                 <div className="container d-flex justify-content-center" style={{backgroundColor: '#F52F41', color: 'white'}}>
@@ -41,16 +58,51 @@ const Home = () => {
                     <span> { user.lastName } Chao</span>
                 </div>
             }
-              <div className="container d-flex justify-content-center" style={{backgroundColor: '#F52F41', color: 'white'}}>
-                  <div className="pt-2 pb-2">
-                    <span className="font-weight-bold"> Bienvenido: </span>
-                    <span> Matias</span>
-                    <span> Chao</span>
+              <div className="container p-4" style={{backgroundColor: '#F52F41', color: 'white'}}>
+                  
+                  <div className="row pb-5">
+                      <div className="col-12 d-flex justify-content-center">
+                        <span className="font-weight-bold"> Bienvenido: </span>
+                        <span className="pl-2 pr-1"> Matias</span>
+                        <span> Chao</span>
+                      </div>
                   </div>
+
+                </div>
+                
+                
+                <div className="row w-100">
+                    
+                    <div className="col-6 text-center mt-4" style={{width: '100%', height: '550px'}}>
+                        <GoogleMapReact
+                            bootstrapURLKeys = {{ key: credentials.mapsKey }}
+                            defaultZoom = { zoom }
+                            defaultCenter = { center }
+                            yesIWantToUseGoogleMapApiInternals
+                            onClick = { _onClick }
+                        >
+                            {
+                                lat && 
+                                <Marker
+                                    lat = { lat }
+                                    lng = { lng }
+                                    name="My Marker"
+                                    color = "#F52F41"
+                                />
+                            }
+                        </GoogleMapReact>
+                    </div>
+
+                    <div className="col-3">
+                        restanrantes
+                        {lat && lat} 
+                        {lng && lng}
+                    </div>
                 </div>
             
         </div>
     )
 }
+
 
 export default Home
